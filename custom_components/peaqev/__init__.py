9df1d9fd3cc2a2760e0f1a52d29213831a97c8db
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-04-07 20:32:21 +0200

    Update init.py. #2

--- custom_components/peaqev/__init__.py
+++ custom_components/peaqev/__init__.py
@@ -27,9 +27,7 @@ async def async_setup_entry(hass: HomeAssistant, config: ConfigEntry) -> bool:
         "chargerid": config.data["chargerid"],
         "nonhours": config.data["options"]["nonhours"],
         "monthlystartpeak": config.data["options"]["startpeaks"],
-        #"cautionhours" : [11,18,19],
-        #"nonhours": [12,16,17],
-        #"monthlystartpeak": {1: 2.0, 2: 1.8, 3: 1.8, 4: 1.5, 5: 1.5, 6: 1.5, 7: 1.5, 8: 1.5, 9: 1.5, 10: 1.5, 11:1.8, 12: 2.0}    
+        "priceaware": config.data["options"]["priceaware"]
     }
 
     hub = Hub(hass, configinputs, DOMAIN)
