9603a0771d93556aad22203ccdbf7c542eeea88c
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-04-10 14:01:59 +0200

    minor

--- custom_components/peaqev/peaqservice/chargertypes/chargertypes.py
+++ custom_components/peaqev/peaqservice/chargertypes/chargertypes.py
@@ -11,19 +11,20 @@ class ChargerTypeData():
         self._charger = None
         self._type = input
         self._hass = hass
-        self._chargerid = chargerid
 
         if input == constants.CHARGERTYPE_CHARGEAMPS:
-            self._charger = ChargeAmps(self._hass, self._chargerid)
+            self._charger = ChargeAmps(self._hass, chargerid)
         elif input == constants.CHARGERTYPE_EASEE:
-            self._charger = Easee(self._hass, self._chargerid)
+            self._charger = Easee(self._hass, chargerid)
 
         self._charger.validatecharger()
 
+    """type returns the implemented chargertype."""
     @property
     def type(self) -> str:
         return self._type
 
+    """charger returns the set charger with all its properties of states, servicecalls etc."""
     @property
     def charger(self):
         return self._charger
