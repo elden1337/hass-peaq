808c15f25011132bcb93125d7751a895d20653aa
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-04-17 17:13:26 +0200

    add new chargetypes for peaks

--- custom_components/peaqev/peaqservice/localetypes/localtypebase.py
+++ custom_components/peaqev/peaqservice/localetypes/localtypebase.py
@@ -1,9 +1,12 @@
+from enum import Enum
 
 
 class LocalTypeBase:
     def __init__(self):
         self._observed_peak = ""
         self._charged_peak = ""
+        self._peakcharge_type = PeakChargeType.Monthly
+        self._peakreader_type = PeakReaderType.Hourly
 
     """Observed peak (only added as sensor if separated from charged peak)"""
     @property
@@ -22,3 +25,25 @@ class LocalTypeBase:
     @charged_peak.setter
     def charged_peak(self, val):
         self._charged_peak = val
+
+    @property
+    def ChargeType(self):
+        return self._peakcharge_type
+
+    @property
+    def ReaderType(self):
+        return self._peakreader_type
+
+
+"""Determine how the peaks are charger. Monthly, weekly, bi-weekly."""
+class PeakChargeType(Enum):
+    Monthly = 1,
+    Weekly = 2,
+    BiWeekly = 3
+
+
+"""Determine how the peaks are read. Hourly to reset every hours, etc"""
+class PeakReaderType(Enum):
+    Hourly = 60,
+    Quarterly = 15,
+    MidHourly = 30,
