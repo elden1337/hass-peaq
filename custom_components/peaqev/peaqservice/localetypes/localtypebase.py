a947e801938c27b9f629fb70cedad74a88dc8754
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-04-20 22:37:36 +0200

    #22, #23, #26 add free_charge_pattern to localetypebase.py

--- custom_components/peaqev/peaqservice/localetypes/localtypebase.py
+++ custom_components/peaqev/peaqservice/localetypes/localtypebase.py
@@ -1,4 +1,18 @@
 from enum import Enum
+from datetime import datetime
+
+"""Determine how the peaks are charger. Monthly, weekly, bi-weekly."""
+class PeakChargeType(Enum):
+    Monthly = 1,
+    Weekly = 2,
+    BiWeekly = 3
+
+
+"""Determine how the peaks are read. Hourly to reset every hours, etc"""
+class PeakReaderType(Enum):
+    Hourly = 60,
+    Quarterly = 15,
+    MidHourly = 30
 
 
 class LocaleTypeBase:
@@ -7,6 +21,7 @@ class LocaleTypeBase:
         self._charged_peak = ""
         self._peakcharge_type = PeakChargeType.Monthly
         self._peakreader_type = PeakReaderType.Hourly
+        self._free_charge_pattern = []
 
     """Observed peak (only added as sensor if separated from charged peak)"""
     @property
@@ -27,23 +42,27 @@ class LocaleTypeBase:
         self._charged_peak = val
 
     @property
-    def ChargeType(self):
+    def current_hour_is_free_charge(self) -> bool:
+        return self._gather_free_charge_pattern()
+
+    @property
+    def ChargeType(self) -> PeakChargeType:
         return self._peakcharge_type
 
     @property
-    def ReaderType(self):
+    def ReaderType(self) -> PeakReaderType:
         return self._peakreader_type
 
+    def _gather_free_charge_pattern(self) -> bool:
+        if len(self._free_charge_pattern) == 0:
+            return False
 
-"""Determine how the peaks are charger. Monthly, weekly, bi-weekly."""
-class PeakChargeType(Enum):
-    Monthly = 1,
-    Weekly = 2,
-    BiWeekly = 3
+        now = datetime.now()
+        for p in self._free_charge_pattern:
+            if now.month in p["M"]:
+                if now.weekday() in p["D"]:
+                    if now.hour in p["H"]:
+                        return True
 
+        return False
 
-"""Determine how the peaks are read. Hourly to reset every hours, etc"""
-class PeakReaderType(Enum):
-    Hourly = 60,
-    Quarterly = 15,
-    MidHourly = 30,
