9e343a6d48b49f44dc10962da56eef4c85c97e25
Magnus Eldén <elden1337@users.noreply.github.com>
2022-05-02 11:22:00 +0200

    Update threshold.py

--- custom_components/peaqev/peaqservice/threshold.py
+++ custom_components/peaqev/peaqservice/threshold.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 import custom_components.peaqev.peaqservice.util.constants as constants
 from peaqevcore.Threshold import ThresholdBase as _core
+import logging
 
+_LOGGER = logging.getLogger(__name__)
 
 class Threshold():
     def __init__(self, hub):
@@ -23,19 +25,22 @@ class Threshold():
 
     @property
     def allowedcurrent(self) -> int:
+        amps = self._setcurrentdict()
+        msg = f"Checking allowed curret against the possible steps: {amps}"
+        _LOGGER.info(msg)
         return _core.allowedcurrent(
             datetime.now().minute,
             self._hub.powersensormovingaverage.value if self._hub.powersensormovingaverage.value is not None else 0,
             self._hub.charger_enabled.value,
             self._hub.charger_done.value,
-            self._setcurrentdict(),
+            amps,
             self._hub.totalhourlyenergy.value,
             self._hub.currentpeak.value
         )
 
     # this one must be done better. Currently cannot accommodate 1-32A single phase for instance.
     def _setcurrentdict(self):
-        if int(self._hub.carpowersensor.value) < 3700 and int(self._hub.carpowersensor.value) > 0:
+        if 0 < int(self._hub.carpowersensor.value) < 3700:
             return constants.CURRENTS_ONEPHASE_1_16
         return constants.CURRENTS_THREEPHASE_1_16
         
