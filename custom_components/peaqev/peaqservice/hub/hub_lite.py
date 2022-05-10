de0720c53e2c4c43ea273b88ff01536bb7957e6f
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-05-10 16:34:59 +0200

    #51 wip

--- custom_components/peaqev/peaqservice/hub/hub_lite.py
+++ custom_components/peaqev/peaqservice/hub/hub_lite.py
@@ -63,11 +63,8 @@ class HubLite(HubBase, HubDataLite):
         return self.currentpeak.value
 
     async def _updatesensor(self, entity, value):
-        # if entity == self.configpower_entity:
-        #     self.power.update(carpowersensor_value=self.carpowersensor.value, total_value=value)
         if entity == self.carpowersensor.entity:
             self.carpowersensor.value = value
-            self.power.update(carpowersensor_value=self.carpowersensor.value, total_value=None)
         elif entity == self.chargerobject.entity:
             self.chargerobject.value = value
         elif entity == self.chargerobject_switch.entity:
