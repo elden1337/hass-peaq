3186f0f839c58e8fb463ad10ce2fe5879e6b0979
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-05-11 15:58:59 +0200

    #57 configflow for light

--- custom_components/peaqev/config_flow.py
+++ custom_components/peaqev/config_flow.py
@@ -15,11 +15,9 @@ from .const import DOMAIN  # pylint:disable=unused-import
 
 _LOGGER = logging.getLogger(__name__)
 
-
-
-LITE_SCHEMA = vol.Schema(
+PEAQTYPE_SCHEMA = vol.Schema(
     {
-        vol.Required(
+        vol.Optional(
             "peaqevtype",
             default="",
         ): vol.In(pk.INSTALLATIONTYPES)
@@ -28,15 +26,13 @@ LITE_SCHEMA = vol.Schema(
 
 SENSOR_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_NAME): cv.string,
+        vol.Optional(CONF_NAME): cv.string,
         vol.Optional("powersensorincludescar", default=False): cv.boolean
     }
 )
 
-INIT_SCHEMA = vol.Schema(
+CHARGER_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_NAME): cv.string,
-        vol.Optional("powersensorincludescar", default=False): cv.boolean,
         vol.Optional(
             "chargertype",
             default="",
@@ -50,49 +46,6 @@ INIT_SCHEMA = vol.Schema(
 )
 
 
-async def _check_power_sensor(hass: HomeAssistant, powersensor: str) -> bool:
-    ret = hass.states.get(powersensor).state
-    try:
-        float(ret)
-        return True
-    except:
-        msg = f"{powersensor} did not produce a valid state for {DOMAIN}. State was {ret}"
-        _LOGGER.error(msg)
-        return False
-
-
-async def _validate_input_first(hass: HomeAssistant, data: dict) -> dict[str, Any]:
-    """ Validate the data can be used to set up a connection."""
-
-    if len(data["chargerid"]) < 3:
-        raise ValueError
-
-    if len(data["name"]) < 3:
-        raise ValueError
-    if not data["name"].startswith("sensor."):
-        data["name"] = f"sensor.{data['name']}"
-    #if await _check_power_sensor(hass, data["name"]) is False:
-    #    raise ValueError
-
-    return {"title": data["name"]}
-
-
-async def _validate_input_first_chargerid(data: dict) -> dict[str, Any]:
-    """ Validate the chargerId"""
-    if len(data["chargerid"]) < 1:
-        raise ValueError
-
-    return {"title": data["name"]}
-
-
-# async def _validate_nonhours(data: dict) -> dict[str, Any]:
-#     """ Validate nonhour-length"""
-#     if len(data["nonhours"]) == 24:
-#         raise ValueError
-
-#     return {"title": data["name"]}
-
-
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
@@ -100,29 +53,35 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     data: Optional[dict[str, Any]]
     info: Optional[dict[str, Any]]
 
-    async def async_step_peaqtype(self, user_input=None):
+    async def async_step_init(self, user_input=None):
+        """Invoked when a user initiates a flow via the user interface."""
         errors = {}
+        _LOGGER.info("entered peaqev config flow!")
         if user_input is not None:
-            pass
+            self.data = user_input
+            if self.data["peaqtype"] == pk.TYPELITE:
+                return await self.async_step_charger()
+            return await self.async_step_sensor()
 
         return self.async_show_form(
-            step_id="peaqtype", data_schema=LITE_SCHEMA, errors=errors, last_step=False
+            step_id="init", data_schema=PEAQTYPE_SCHEMA, errors=errors, last_step=False
         )
 
     async def async_step_sensor(self, user_input=None):
         errors = {}
         if user_input is not None:
-            pass
+            self.data.update(user_input)
+            return await self.async_step_charger()
 
         return self.async_show_form(
             step_id="sensor", data_schema=SENSOR_SCHEMA, errors=errors, last_step=False
         )
 
-    async def async_step_user(self, user_input=None):
+    async def async_step_charger(self, user_input=None):
         errors = {}
         if user_input is not None:
             try:
-                self.info = await _validate_input_first(self.hass, user_input)
+                self.info = await _validate_input_first(user_input)
             except ValueError:
                 errors["base"] = "invalid_powersensor"
             try:
@@ -131,12 +90,38 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "invalid_chargerid"
             if not errors:
                 self.data = user_input
-                #self.data[self.OPTIONS] = []
-                return await self.async_step_hours()
-                #return self.async_create_entry(title=self.info["title"], data=self.data)
+                return await self.async_step_priceaware()
+
 
         return self.async_show_form(
-            step_id="user", data_schema=INIT_SCHEMA, errors=errors, last_step=False
+            step_id="charger", data_schema=CHARGER_SCHEMA, errors=errors, last_step=False
+        )
+
+    async def async_step_priceaware(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            self.data.update(user_input)
+            if str(self.data["priceaware"]) == "false":
+                return await self.async_step_hours()
+            return await self.async_step_months()
+
+        _priceaware = False
+        _absolute_top_price = 0
+        #_cautionhour_type = pk.CAUTIONHOURTYPE_INTERMEDIATE
+
+        return self.async_show_form(
+            step_id="priceaware",
+            data_schema=vol.Schema(
+                {
+                    vol.Optional("priceaware", default=_priceaware): cv.boolean,
+                    vol.Optional("absolute_top_price", default=_absolute_top_price): cv.positive_float,
+                    vol.Optional(
+                        "cautionhour_type",
+                        default=pk.CAUTIONHOURTYPE_INTERMEDIATE,
+                    ): vol.In(pk.CAUTIONHOURTYPE_NAMES),
+                }),
+            errors=errors,
+            last_step=False,
         )
 
     async def async_step_hours(self, user_input=None):
@@ -148,9 +133,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         _transfer_cautionhours = mockhours
         _transfer_nonhours = mockhours
-        _priceaware = False
-        _absolute_top_price = 0
-        _cautionhour_type = pk.CAUTIONHOURTYPE_INTERMEDIATE
 
         return self.async_show_form(
             step_id="hours",
@@ -161,13 +143,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 ),
                 vol.Optional("cautionhours", default=list(_transfer_cautionhours)): cv.multi_select(
                     mockhours
-                ),
-                vol.Optional("priceaware", default=_priceaware): cv.boolean,
-                vol.Optional("absolute_top_price", default=_absolute_top_price): cv.positive_float,
-                vol.Optional(
-                    "cautionhour_type",
-                    default=pk.CAUTIONHOURTYPE_INTERMEDIATE,
-                ): vol.In(pk.CAUTIONHOURTYPE_NAMES),
+                )
             }),
             last_step=False,
         )
@@ -207,94 +183,40 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             last_step=True,
         )
 
-    # @staticmethod
-    # @callback
-    # def async_get_options_flow(config_entry):
-    #     return OptionsFlowHandler(config_entry)
-
-# class OptionsFlowHandler(config_entries.OptionsFlow):
-#
-#     def __init__(self, config_entry):
-#         self.config_entry = config_entry
-#         self.options = dict(config_entry.options)
-#
-#     async def async_step_init(self, user_input=None):
-#         return await self.async_step_hours()
-#
-#     async def async_step_hours(self, user_input=None):
-#         if user_input is not None:
-#             self.options.update(user_input)
-#             return await self.async_step_startpeaks()
-#
-#         mockhours = [h for h in range(0, 24)]
-#
-#         _transfer_cautionhours = self.config_entry.options["cautionhours"] if "cautionhours" in self.config_entry.options.keys() else self.config_entry.data["options"]["cautionhours"] if "cautionhours" in self.config_entry.data["options"].keys() else mockhours
-#         _transfer_nonhours = self.config_entry.options["nonhours"] if "nonhours" in self.config_entry.options.keys() else self.config_entry.data["options"]["nonhours"] if "nonhours" in self.config_entry.data["options"].keys() else mockhours
-#         _priceaware = self.config_entry.options["priceaware"] if "priceaware" in self.config_entry.options.keys() else False
-#         _absolute_top_price = self.config_entry.options["absolute_top_price"] if "absolute_top_price" in self.config_entry.options.keys() else None
-#         _cautionhour_type = self.config_entry.options["cautionhour_type"] if "cautionhour_type" in self.config_entry.options.keys() else pk.CAUTIONHOURTYPE_INTERMEDIATE
-#
-#         return self.async_show_form(
-#             step_id="hours",
-#             data_schema=vol.Schema(
-#             {
-#                 vol.Optional("nonhours", default=list(_transfer_nonhours)): cv.multi_select(
-#                     mockhours
-#                 ),
-#                 vol.Optional("cautionhours", default=list(_transfer_cautionhours)): cv.multi_select(
-#                     mockhours
-#                 ),
-#                 vol.Optional("priceaware", default=_priceaware): cv.boolean,
-#                 vol.Optional("absolute_top_price", default=_absolute_top_price): cv.positive_float,
-#                 vol.Optional(
-#                     "cautionhour_type",
-#                     default=pk.CAUTIONHOURTYPE_INTERMEDIATE,
-#                 ): vol.In(pk.CAUTIONHOURTYPE_NAMES),
-#             }),
-#             last_step=False,
-#         )
-#
-#     async def async_step_startpeaks(self, user_input=None):
-#         if user_input is not None:
-#             months = await OptionsFlowHandler._set_startpeak_dict(user_input)
-#             self.options.update(months)
-#             return await self._update_options()
-#
-#         months = [m for m in range(1, 13)]
-#
-#         mock_startpeaks = {}
-#         for m in months:
-#             _m = str(m)
-#             mock_startpeaks[_m] = 1.5
-#
-#         _transfer_startpeaks = self.config_entry.options["startpeaks"] if "startpeaks" in self.config_entry.options.keys() else self.config_entry.data["options"]["startpeaks"] if "startpeaks" in self.config_entry.data["options"].keys() else mock_startpeaks
-#
-#         return self.async_show_form(
-#             step_id="startpeaks",
-#             data_schema=vol.Schema(
-#                 {
-#                 vol.Optional("jan", default=_transfer_startpeaks["1"]): cv.positive_float,
-#                 vol.Optional("feb", default=_transfer_startpeaks["2"]): cv.positive_float,
-#                 vol.Optional("mar", default=_transfer_startpeaks["3"]): cv.positive_float,
-#                 vol.Optional("apr", default=_transfer_startpeaks["4"]): cv.positive_float,
-#                 vol.Optional("may", default=_transfer_startpeaks["5"]): cv.positive_float,
-#                 vol.Optional("jun", default=_transfer_startpeaks["6"]): cv.positive_float,
-#                 vol.Optional("jul", default=_transfer_startpeaks["7"]): cv.positive_float,
-#                 vol.Optional("aug", default=_transfer_startpeaks["8"]): cv.positive_float,
-#                 vol.Optional("sep", default=_transfer_startpeaks["9"]): cv.positive_float,
-#                 vol.Optional("oct", default=_transfer_startpeaks["10"]): cv.positive_float,
-#                 vol.Optional("nov", default=_transfer_startpeaks["11"]): cv.positive_float,
-#                 vol.Optional("dec", default=_transfer_startpeaks["12"]): cv.positive_float
-#                 }),
-#             last_step=True,
-#         )
-#
-#     async def _update_options(self):
-#         """Update config entry options."""
-#         return self.async_create_entry(title="", data=self.options)
-
 
 async def _set_startpeak_dict(user_input) -> dict:
     return {1: user_input["jan"], 2: user_input["feb"], 3: user_input["mar"], 4: user_input["apr"],
            5: user_input["may"], 6: user_input["jun"], 7: user_input["jul"], 8: user_input["aug"],
            9: user_input["sep"], 10: user_input["oct"], 11: user_input["nov"], 12: user_input["dec"]}
+
+async def _check_power_sensor(hass: HomeAssistant, powersensor: str) -> bool:
+    ret = hass.states.get(powersensor).state
+    try:
+        float(ret)
+        return True
+    except Exception as e:
+        msg = f"{powersensor} did not produce a valid state for {DOMAIN}. State was {ret}. Ex: {e}"
+        _LOGGER.error(msg)
+        return False
+
+async def _validate_input_first(data: dict) -> dict[str, Any]:
+    """ Validate the data can be used to set up a connection."""
+
+    if len(data["chargerid"]) < 3:
+        raise ValueError
+
+    if len(data["name"]) < 3:
+        raise ValueError
+    if not data["name"].startswith("sensor."):
+        data["name"] = f"sensor.{data['name']}"
+    #if await _check_power_sensor(hass, data["name"]) is False:
+    #    raise ValueError
+
+    return {"title": data["name"]}
+
+async def _validate_input_first_chargerid(data: dict) -> dict[str, Any]:
+    """ Validate the chargerId"""
+    if len(data["chargerid"]) < 1:
+        raise ValueError
+
+    return {"title": data["name"]}
