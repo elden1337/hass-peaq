088ab59875a192149ea4061d8768cfd8d5723c90
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-06-08 19:59:45 +0200

    #101 implement call_wait_timer on charger.py

--- custom_components/peaqev/peaqservice/charger/charger.py
+++ custom_components/peaqev/peaqservice/charger/charger.py
@@ -19,7 +19,7 @@ from custom_components.peaqev.peaqservice.util.constants import (
 )
 
 _LOGGER = logging.getLogger(__name__)
-
+CALL_WAIT_TIMER = 60
 
 class Charger:
     def __init__(self, hub, hass, servicecalls):
@@ -29,6 +29,7 @@ class Charger:
         self._charger_stopped = False
         self._service_calls = servicecalls
         self._session_is_active = False
+        self._latest_charger_call = 0
         self._session = Session(self)
 
     async def charge(self):
@@ -75,15 +76,17 @@ class Charger:
             await self._call_charger(PAUSE)
 
     async def _call_charger(self, command: str):
-        calls = self._service_calls.get_call(command)
-        _LOGGER.info(calls[DOMAIN], calls[command], calls["params"])
-        result = await self._hub.hass.services.async_call(
-            calls[DOMAIN],
-            calls[command],
-            calls["params"]
-        )
-        msg = f"Calling charger {command}, result {result}"
-        _LOGGER.info(msg)
+        if time.time() - self._latest_charger_call > CALL_WAIT_TIMER:
+            calls = self._service_calls.get_call(command)
+            _LOGGER.debug(calls[DOMAIN], calls[command], calls["params"])
+            await self._hub.hass.services.async_call(
+                calls[DOMAIN],
+                calls[command],
+                calls["params"]
+            )
+            msg = f"Calling charger {command}"
+            _LOGGER.debug(msg)
+            self._latest_charger_call = time.time()
 
     async def _updatemaxcurrent(self):
         """If enabled, let the charger periodically update it's current during charging."""
