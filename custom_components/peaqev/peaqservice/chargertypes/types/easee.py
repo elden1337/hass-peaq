740d56f4eb7e912c9fa961d4e2fc8f68e59fcbdb
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-06-13 19:59:16 +0200

    7A on starting/resuming easee to let equalizer understand it's charging.

--- custom_components/peaqev/peaqservice/chargertypes/types/easee.py
+++ custom_components/peaqev/peaqservice/chargertypes/types/easee.py
@@ -75,7 +75,7 @@ class Easee(ChargerBase):
 
         _on = CallType("start", _on_off_params)
         _off = CallType("stop", _on_off_params)
-        _resume = CallType("set_charger_dynamic_limit", {"current": "6", "charger_id": self._chargerid})
+        _resume = CallType("set_charger_dynamic_limit", {"current": "7", "charger_id": self._chargerid})
         _pause = CallType("set_charger_dynamic_limit", {"current": "0", "charger_id": self._chargerid})
 
         self._set_servicecalls(
