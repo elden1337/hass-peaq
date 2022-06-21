9662a17eb7a7d9993685107d11895fb31de02699
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-06-21 22:38:18 +0200

    #116

--- custom_components/peaqev/peaqservice/charger/charger.py
+++ custom_components/peaqev/peaqservice/charger/charger.py
@@ -30,12 +30,15 @@ class Charger:
         self._service_calls = servicecalls
         self._session_is_active = False
         self._latest_charger_call = 0
+        self._repeat_is_running = False
         self._session = Session(self)
 
     async def charge(self):
         """Main function to turn charging on or off"""
         if self._hub.chargecontroller.status is CHARGERSTATES.Disabled.name:
             return
+        if self._repeat_is_running:
+            self._is_running(False)
         if self._hub.charger_enabled.value is True:
             if self._hub.chargecontroller.status is CHARGERSTATES.Start.name:
                 if self._chargertype_charger_is_on is False and self._charger_running is False:
@@ -161,10 +164,8 @@ class Charger:
         timer = 180
         start_time = time.time()
         self._hub.chargerobject_switch.updatecurrent()
-
         while time.time() - start_time < timer:
             time.sleep(3)
-
         self._hub.chargerobject_switch.updatecurrent()
 
     def _is_running(self, determinator: bool):
@@ -178,4 +179,7 @@ class Charger:
             if charger not in chargingstates:
                 self._charger_running = False
                 self._charger_stopped = True
+                self._repeat_is_running = False
                 _LOGGER.debug("charger-class has been stopped")
+            else:
+                self._repeat_is_running = True
