d3c8bf490520466a4af55ed502f851c1c732efa4
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-07-03 23:08:59 +0200

    #66 wip

--- custom_components/peaqev/peaqservice/hub/scheduler/schedule.py
+++ custom_components/peaqev/peaqservice/hub/scheduler/schedule.py
@@ -6,8 +6,22 @@ class Schedule:
             departure_time:str,
             schedule_starttime:str = None,
             override_settings:bool = False
-    ):
+    ) -> bool:
         pass
 
     async def _parse_data(self):
+        #parse the datetime(s)
+        #if the starttime is None, set it to now()
         pass
+
+
+    async def _inspect_hours(self):
+        """inspect the hourly pricing"""
+        pass
+
+    async def _make_calulation(self):
+        """calculate based on the pricing of hours, current peak and the avg24hr energy consumption"""
+        pass
+
+
+    #todo: make new averagesensor which stores average 24h for the house (to help scheduler be as accurate as possible)
