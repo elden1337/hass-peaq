03cf3cd4fe48644910a5e860abd278dc71be5e78
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-11-01 13:53:11 +0100

    #158

--- custom_components/peaqev/binary_sensor.py
+++ custom_components/peaqev/binary_sensor.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.core import HomeAssistant
-from peaqevcore.models.hub.const import CHARGERENABLED, CHARGERDONE
+from peaqevcore.models.hub.const import CHARGERDONE
 
 from .const import (
     DOMAIN)
@@ -16,36 +16,9 @@ async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entitie
     peaqsensors = []
 
     peaqsensors.append(PeaqBinarySensorDone(hub))
-    peaqsensors.append(PeaqBinarySensorEnabled(hub))
     async_add_entities(peaqsensors)
 
 SCAN_INTERVAL = timedelta(seconds=4)
-
-
-class PeaqBinarySensorEnabled(BinarySensorEntity):
-    """The binary sensor for peaq being enabled or disabled"""
-    def __init__(self, hub) -> None:
-        """Initialize a Peaq Binary sensor."""
-        self._attr_name = f"{hub.hubname} {CHARGERENABLED}"
-        self._hub = hub
-        self._attr_device_class = "none"
-
-    @property
-    def unique_id(self):
-        """The unique id used by Home Assistant"""
-        return f"{DOMAIN.lower()}_{self._attr_name}"
-
-    @property
-    def device_info(self):
-        return {"identifiers": {(DOMAIN, self._hub.hub_id)}}
-
-    @property
-    def is_on(self) -> bool:
-        try:
-            return self._hub.sensors.charger_enabled.value
-        except:
-            _LOGGER.debug("Binarysensor_enabled could not get state from hub.")
-            return False
 
 
 class PeaqBinarySensorDone(BinarySensorEntity):
