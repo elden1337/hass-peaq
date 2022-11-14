bc8e8e4ee52fd8586f5a4163369a7dacc72643f3
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-11-14 10:11:48 +0100

    #177 add restoresensor with nordpool-average

--- custom_components/peaqev/peaqservice/hub/nordpool.py
+++ custom_components/peaqev/peaqservice/hub/nordpool.py
@@ -1,11 +1,12 @@
 import logging
+from statistics import mean
 
 import homeassistant.helpers.template as template
 
 _LOGGER = logging.getLogger(__name__)
 
 NORDPOOL = "nordpool"
-
+AVERAGE_MAX_LEN = 31
 
 class NordPoolUpdater:
     def __init__(self, hass, hub, is_active: bool = True):
@@ -16,6 +17,7 @@ class NordPoolUpdater:
         self.prices_tomorrow: list = []
         self.state: float = 0
         self.nordpool_entity: str = ""
+        self.average_data: list = []
         if is_active:
             self._setup_nordpool()
 
@@ -39,6 +41,11 @@ class NordPoolUpdater:
             self.state = ret.state
             self._hub.hours.prices = self.prices
             self._hub.hours.prices_tomorrow = self.prices_tomorrow
+            try:
+                _avg_data = str(ret.attributes.get("average"))
+                self.add_average_data(float(_avg_data))
+            except Exception as ee:
+                _LOGGER.warning(f"Could not parse today's average from Nordpool. {ee}")
         elif self._hub.is_initialized:
             _LOGGER.error("Could not get nordpool-prices")
 
@@ -57,3 +64,30 @@ class NordPoolUpdater:
         except Exception as e:
             self._hub.options.price.price_aware = False
             _LOGGER.error(f"Peaqev was unable to get a Nordpool-entity. Disabling Priceawareness until reboot of HA: {e}")
+
+    def import_average_data(self, incoming: list):
+        if isinstance(incoming, list):
+            if len(incoming) > 0:
+                self.average_data = incoming
+        self._cap_average_data_length()
+
+    def add_average_data(self, new_val):
+        if isinstance(new_val, float):
+            if len(self.average_data) == 0:
+                self.average_data.append(new_val)
+            elif self.average_data[-1] != new_val:
+                self.average_data.append(new_val)
+            self._cap_average_data_length()
+
+    def _cap_average_data_length(self):
+        while len(self.average_data) > AVERAGE_MAX_LEN:
+            del self.average_data[0]
+
+    def get_average(self, days: int) -> float:
+        if len(self.average_data) > days:
+            ret = self.average_data[-days]
+        elif len(self.average_data) == 0:
+            return 0.0
+        else:
+            ret = self.average_data
+        return round(mean(ret),2)
--- custom_components/peaqev/sensors/money_sensor.py
+++ custom_components/peaqev/sensors/money_sensor.py
@@ -1,12 +1,14 @@
 import logging
 from datetime import datetime
 
+from homeassistant.helpers.restore_state import RestoreEntity
+
 from custom_components.peaqev.peaqservice.util.constants import HOURCONTROLLER
 from custom_components.peaqev.sensors.sensorbase import SensorBase
 
 _LOGGER = logging.getLogger(__name__)
 
-class PeaqMoneySensor(SensorBase):
+class PeaqMoneySensor(SensorBase, RestoreEntity):
     """Special sensor which is only created if priceaware is true"""
     def __init__(self, hub, entry_id):
         name = f"{hub.hubname} {HOURCONTROLLER}"
@@ -22,6 +24,8 @@ class PeaqMoneySensor(SensorBase):
         self._current_peak = None
         self._avg_cost = None
         self._max_charge = None
+        self._average_nordpool = None
+        self._average_nordpool_data = []
 
     @property
     def state(self):
@@ -40,6 +44,8 @@ class PeaqMoneySensor(SensorBase):
         self._current_peak = self._hub.sensors.current_peak.value
         self._avg_cost = f"{self._hub.hours.get_average_kwh_price()} {self._currency}"
         self._max_charge = f"{self._hub.hours.get_total_charge()} kWh"
+        self._average_nordpool = self._hub.nordpool.get_average(7)
+        self._average_nordpool_data = self._hub.nordpool.average_data
 
     @property
     def extra_state_attributes(self) -> dict:
@@ -48,10 +54,19 @@ class PeaqMoneySensor(SensorBase):
             "Caution hours": self.set_dynamic_caution_hours_display(),
             "Current hour charge permittance": self.set_charge_permittance_display(),
             "Avg price per kWh": self._avg_cost,
-            "Max charge amount": self._max_charge
+            "Max charge amount": self._max_charge,
+            "Nordpool average 7 days": self._average_nordpool,
+            "Nordpool average data": self._average_nordpool_data
         }
-
         return attr_dict
+
+    async def async_added_to_hass(self):
+        state = await super().async_get_last_state()
+        if state:
+            self._hub.nordpool.import_average_data(state.attributes.get('Nordpool average data', 50))
+            self._average_nordpool = self._hub.nordpool.get_average(7)
+        else:
+            self._average_nordpool = "-"
 
     def _get_written_state(self) -> str:
         hour = datetime.now().hour
