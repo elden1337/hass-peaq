b69c2386739395d24180e9dec4eebf66f71805a9
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-12-11 22:39:42 +0100

    #179 add max-amps for threephase and onephase

--- custom_components/peaqev/peaqservice/hub/power_canary.py
+++ custom_components/peaqev/peaqservice/hub/power_canary.py
@@ -1,7 +1,8 @@
 # from peaqevcore.models.fuses import Fuses
 from enum import Enum
 
-from peaqevcore.models.const import CURRENTS_THREEPHASE_1_32, CURRENTS_THREEPHASE_1_16
+from peaqevcore.models.const import CURRENTS_THREEPHASE_1_32, CURRENTS_THREEPHASE_1_16, CURRENTS_ONEPHASE_1_16, \
+    CURRENTS_ONEPHASE_1_32
 
 
 class Fuses(Enum):
@@ -39,14 +40,17 @@ WARNING_THRESHOLD = 0.75
 
 
 class PowerCanary:
+    warning_threshold: float = WARNING_THRESHOLD
+    cutoff_threshold: float = CUTOFF_THRESHOLD
+
     def __init__(self, hub):
+        self._enabled: bool = False
+        self._active: bool = False
         self._hub = hub
         self._fuse = Fuses.parse_from_config(hub.options.fuse_type)
         self._fuse_max = FUSES_DICT[self._fuse] if self._fuse is not None else 0
-        # self._current_amps: int = 0
-        # self._current_car_power: int = 0
-        self._threephase_amps: dict = self._set_allowed_threephase_amps()
-        self._enabled: bool = False
+        self._threephase_amps: dict = self._set_allowed_amps(CURRENTS_THREEPHASE_1_32, CURRENTS_THREEPHASE_1_16)
+        self._onephase_amps: dict = self._set_allowed_amps(CURRENTS_ONEPHASE_1_32, CURRENTS_ONEPHASE_1_16)
         self._allow_amp_adjustment: bool = self._hub.chargertype.charger.servicecalls.options.allowupdatecurrent
         self._validate()
 
@@ -61,14 +65,6 @@ class PowerCanary:
     @property
     def active(self) -> bool:
         return self._active
-
-    @property
-    def warning_threshold(self) -> float:
-        return WARNING_THRESHOLD
-
-    @property
-    def cutoff_threshold(self) -> float:
-        return CUTOFF_THRESHOLD
 
     @property
     def alive(self) -> bool:
@@ -93,25 +89,39 @@ class PowerCanary:
             return f"Ok"
 
     @property
-    def threephase_amps(self) -> dict:
-        return self._get_currently_allowed_threephase_amps()
+    def onephase_amps(self) -> dict:
+        return self._get_currently_allowed_amps(self._onephase_amps)
 
-    def allow_adjustment(self, new_amps: int) -> bool:
+    @property
+    def threephase_amps(self) -> dict:
+        return self._get_currently_allowed_amps(self._threephase_amps)
+
+    def allow_adjustment(self, current_amps: int, new_amps: int, phase) -> bool:
         """this method returns true if the desired adjustment 'new_amps' is not breaching threshold"""
         if not self._allow_amp_adjustment:
             return False
-        return False
+        _new_power = self._get_power_from_amps(current_amps, new_amps, phase)
+        return all(
+            [
+                self._hub.sensors.power.total.value + _new_power < self._fuse_max * CUTOFF_THRESHOLD,
+                self._hub.sensors.power.car.value + _new_power + self._hub.sensors.powersensormovingaverage.value < self._fuse_max * CUTOFF_THRESHOLD,
+            ]
+        )
 
-    def _get_currently_allowed_threephase_amps(self) -> dict:
+    def _get_power_from_amps(self, current_amps, new_amps, phases) -> int:
+        #get from dict and diff it. check based on phase-charging.
+        return 5
+
+    def _get_currently_allowed_amps(self, amps) -> dict:
         """get the currently allowed amps based on the current power draw"""
         _max = (self._fuse_max * CUTOFF_THRESHOLD)
-        return {k: v for (k, v) in self._threephase_amps.items() if k+self._hub.sensors.power.total.value < _max}
+        return {k: v for (k, v) in amps.items() if k + self._hub.sensors.power.total.value < _max}
 
-    def _set_allowed_threephase_amps(self) -> dict:
-        """only allow amps 16-32 threephase if user has set this value high enough"""
+    def _set_allowed_amps(self, amps_dict, default_amps) -> dict:
+        """only allow amps if user has set this value high enough"""
         if self._fuse_max > 0:
-            return {k: v for (k, v) in CURRENTS_THREEPHASE_1_32.items() if k < self._fuse_max}
-        return CURRENTS_THREEPHASE_1_16
+            return {k: v for (k, v) in amps_dict.items() if k < self._fuse_max}
+        return default_amps
 
     def _validate(self):
         if self._fuse_max == 0:
@@ -123,5 +133,4 @@ class PowerCanary:
     """
     1 if trying to raise amps and they would hit mains-treshold, dont raise
     2 if approaching mains-threshold on current amps. Lower if possible, else turn off
-    3 only allow amps 16-32 threephase if user has set this value high enough
     """
