d69d16e036ff98714185d86d8f8e702b74fd72b9
Magnus Eldén <magnus.elden@outlook.com>
2023-02-16 22:36:34 +0100

    wip callback boilerplate

--- custom_components/peaqev/peaqservice/hub/callback.py
+++ custom_components/peaqev/peaqservice/hub/callback.py
@@ -1,12 +1,39 @@
-class Callback:
+class ObserverDirector:
+    _observers: dict = {}
+    
+    @staticmethod
+    def attach(event_type: str, func) -> None:
+        if event_type in ObserverDirector._observers:
+            ObserverDirector._observers[event_type].append(func)
+        ObserverDirector._observers[event_type] = [func]
+
+    @staticmethod
+    def notify(event_type, *args) -> None:
+        try:
+            for o in ObserverDirector._observers[event_type]:
+                o(args)
+        except:
+            pass
+
+
+class testclass:
     def __init__(self):
-        self._observers: list = []
+        ObserverDirector.attach("even 8", self.notified)
+        ObserverDirector.attach("molusk", self.notified)
 
-    def callback(self):
-        for callback in self._observers:
-            # print('announcing change')
-            callback()
+    def notified(self, *args):
+        print(f"I was notified {args}")
 
-    def bind_to(self, callback):
-        # print(f'bound {id(self)}')
-        self._observers.append(callback)
+
+class test_handling:
+    def __init__(self):
+        for i in range(0,100):
+            if i%8 == 0:
+                ObserverDirector.notify("even 8", i)
+
+
+t = testclass()
+tt = test_handling()
+ObserverDirector.notify("molusk", "Magnus")
+
+
