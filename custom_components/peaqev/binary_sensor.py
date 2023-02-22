6f81af118d5e0c2239083b720fa8c5a11ec23e2f
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-02-22 21:57:44 +0100

    await bug

--- custom_components/peaqev/binary_sensor.py
+++ custom_components/peaqev/binary_sensor.py
@@ -12,7 +12,7 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_entities):  # pylint:disable=unused-argument
     hub = hass.data[DOMAIN]["hub"]
 
-    peaqsensors = helper.gather_binary_sensors(hub)
+    peaqsensors = await helper.gather_binary_sensors(hub)
     async_add_entities(peaqsensors)
 
 SCAN_INTERVAL = timedelta(seconds=4)
