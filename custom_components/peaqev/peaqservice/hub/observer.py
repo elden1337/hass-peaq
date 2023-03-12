2d3e63c352f2a4a19356cf39b798d91d8c64b6f9
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-03-12 09:39:27 +0100

    fixes #210

--- custom_components/peaqev/peaqservice/hub/observer.py
+++ custom_components/peaqev/peaqservice/hub/observer.py
@@ -50,7 +50,7 @@ class Observer:
         if self._ok_to_broadcast(command[0]):
             if command[1] > time.time():
                 for func in self._subscribers[command[0]]:
-                    if len(command) == 3:
+                    if command[2] is not None:
                         func(command[2])
                     else:
                         func()
