8e6cc388a5f436c0172e594430891d9b4e12a8e2
Magnus Eldén <magnus.elden@outlook.com>
2023-03-15 10:47:29 +0100

    #208

--- custom_components/peaqev/sensors/gain_loss_sensor.py
+++ custom_components/peaqev/sensors/gain_loss_sensor.py
@@ -15,6 +15,7 @@ class GainLossSensor(SensorEntity):
         self._entry_id = entry_id
         self._attr_name = f"{self._hub.hubname} gain/loss {timeperiod.value.lower()}"
         self._state = None
+        self._raw_state = None
         self._timeperiod = timeperiod
         self._attr_icon = "mdi:cash-clock"
         self._attr_available = True
@@ -30,23 +31,21 @@ class GainLossSensor(SensorEntity):
 
     def update(self) -> None:
         ret = self._hub.gainloss.state(self._timeperiod)
-        if self._state is None:
-            self._state = round(ret * 100,3)
-        elif self._state != round(ret * 100,3):
-            self._state = round(ret * 100, 3)
+        if self._state != round(ret * 100,1):
+            self._state = round(ret * 100, 1)
+            self._raw_state = ret
 
     @property
     def unique_id(self):
         """Return a unique ID to use for this sensor."""
-        return f"{DOMAIN}_{self._entry_id}_{nametoid(self._attr_name)}"
+        return f"{DOMAIN}_{self._entry_id}_{nametoid(self._attr_name)}" 
 
     @property
     def device_info(self):
         return {"identifiers": {(DOMAIN, self._hub.hub_id, POWERCONTROLS)}}
 
-    # @property
-    # def extra_state_attributes(self) -> dict:
-    #     return {
-    #         "warning_threshold": self._warning,
-    #         "cutoff_threshold":  self._cutoff
-    #     }
+    @property
+    def extra_state_attributes(self) -> dict:
+        return {
+            "raw_state": self._raw_state
+        }
