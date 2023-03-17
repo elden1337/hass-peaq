fd9cb167f6a48f2d7bb4cc5ece5e236ebe360652
Magnus Eldén <magnus.elden@outlook.com>
2023-03-17 08:54:54 +0100

    remove junk

--- custom_components/peaqev/peaqservice/chargecontroller/ichargecontroller.py
+++ custom_components/peaqev/peaqservice/chargecontroller/ichargecontroller.py
@@ -214,37 +214,3 @@ class IChargeController:
     @abstractmethod
     def _get_status_connected(self, charger_state=None) -> ChargeControllerStates:
         pass
-
-
-    # @staticmethod
-    # def _defer_start(non_hours: list) -> bool:
-    #     """Defer starting if next hour is a non-hour and minute is 50 or greater, to avoid short running times."""
-    #     if (datetime.now() + timedelta(hours=1)).hour in non_hours:
-    #         return datetime.now().minute >= 50
-    #     return False
-    #
-    # @staticmethod
-    # def _calculate_stop_len(nonhours) -> str:
-    #     ret = ""
-    #     for idx, h in enumerate(nonhours):
-    #         if idx + 1 < len(nonhours):
-    #             if IChargeController._getuneven(nonhours[idx + 1], nonhours[idx]):
-    #                 ret = IChargeController._get_stopped_string(h)
-    #                 break
-    #         elif idx + 1 == len(nonhours):
-    #             ret = IChargeController._get_stopped_string(h)
-    #             break
-    #     return ret
-    #
-    # @staticmethod
-    # def _get_stopped_string(h) -> str:
-    #     val = h + 1 if h + 1 < 24 else h + 1 - 24
-    #     if len(str(val)) == 1:
-    #         return f"Charging stopped until 0{val}:00"
-    #     return f"Charging stopped until {val}:00"
-    #
-    # @staticmethod
-    # def _getuneven(first, second) -> bool:
-    #     if second > first:
-    #         return first - (second - 24) != 1
-    #     return first - second != 1
