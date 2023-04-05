d020205359364aa7c7fdd60e62f4884282b519e7
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-04-05 22:43:21 +0200

    provoke line length

--- custom_components/peaqev/peaqservice/chargecontroller/chargecontroller_lite.py
+++ custom_components/peaqev/peaqservice/chargecontroller/chargecontroller_lite.py
@@ -2,12 +2,9 @@ import logging
 
 from peaqevcore.models.chargecontroller_states import ChargeControllerStates
 
-from custom_components.peaqev.peaqservice.chargecontroller.chargecontroller_helpers import \
-    async_defer_start
-from custom_components.peaqev.peaqservice.chargecontroller.const import \
-    INITIALIZING
-from custom_components.peaqev.peaqservice.chargecontroller.ichargecontroller import \
-    IChargeController
+from custom_components.peaqev.peaqservice.chargecontroller.chargecontroller_helpers import async_defer_start
+from custom_components.peaqev.peaqservice.chargecontroller.const import INITIALIZING
+from custom_components.peaqev.peaqservice.chargecontroller.ichargecontroller import IChargeController
 
 _LOGGER = logging.getLogger(__name__)
 
