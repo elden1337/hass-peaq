c45dcfc76fc0d2d8e95a2ec55c3888348a89d309
Magnus Eldén <magnus.elden@outlook.com>
2023-04-15 14:08:46 +0200

    not building. refactor observer

--- custom_components/peaqev/peaqservice/hub/observer/observer_coordinator.py
+++ custom_components/peaqev/peaqservice/hub/observer/observer_coordinator.py
@@ -1,3 +1,127 @@
+# from __future__ import annotations
+
+# import asyncio
+# import logging
+# import time
+# from typing import Callable
+# from custom_components.peaqev.peaqservice.util.extensionmethods import async_iscoroutine
+# from custom_components.peaqev.peaqservice.hub.observer.const import (
+#     COMMAND_WAIT,
+#     TIMEOUT,
+# )
+# from custom_components.peaqev.peaqservice.hub.observer.models.command import Command
+# from custom_components.peaqev.peaqservice.hub.observer.models.function_call import (
+#     FunctionCall,
+# )
+# from custom_components.peaqev.peaqservice.hub.observer.models.observer_model import (
+#     ObserverModel,
+# )
+
+# _LOGGER = logging.getLogger(__name__)
+
+
+# class Observer:
+#     """
+#     Observer class handles updates throughout peaqev.
+#     Attach to hub class and subscribe to updates (string matches) in other classes connected to the hub.
+#     When broadcasting, you may use one argument that the of-course needs to correspond to your receiving function.
+#     """
+
+#     def __init__(self, hub):
+#         self.model = ObserverModel()
+#         self.hub = hub
+
+#     def activate(self, init_broadcast: str = None) -> None:
+#         self.model.active = True
+#         if init_broadcast is not None:
+#             self.broadcast(init_broadcast)
+
+#     def deactivate(self) -> None:
+#         self.model.active = False
+
+#     def add(self, command: str, func, _async: bool = False):
+#         if command in self.model.subscribers.keys():
+#             self.model.subscribers[command].append(FunctionCall(func, _async))
+#         else:
+#             self.model.subscribers[command] = [FunctionCall(func, _async)]
+
+#     async def async_broadcast(self, command: str, argument=None):
+#         await self.hub.state_machine.async_add_executor_job(
+#             self.broadcast, command, argument
+#         )
+
+#     def broadcast(self, command: str, argument=None):
+#         _expiration = time.time() + TIMEOUT
+#         cc = Command(command, _expiration, argument)
+#         if cc not in self.model.broadcast_queue:
+#             self.model.broadcast_queue.append(cc)
+#         # if self.model.active:
+#         q: Command
+#         for q in self.model.broadcast_queue:
+#             if q.command in self.model.subscribers.keys():
+#                 self._dequeue_and_broadcast(q)
+
+#     def _dequeue_and_broadcast(self, command: Command):
+#         _LOGGER.debug(
+#             f"ready to broadcast: {command.command} with params: {command.argument}"
+#         )
+#         if self._ok_to_broadcast(command.command):
+#             if command.expiration > time.time():
+#                 func: FunctionCall
+#                 for func in self.model.subscribers[command.command]:
+#                     if func.call_async:
+#                         _ = asyncio.run_coroutine_threadsafe(
+#                             self.async_call_func(func.function, command),
+#                             self.hub.state_machine.loop,
+#                         ).result()
+#                     else:
+#                         self._call_func(func.function, command)
+#                 self.model.broadcast_queue.remove(command)
+#             else:
+#                 _LOGGER.debug(f"broadcast timed out: {command}")
+#                 self.model.broadcast_queue.remove(command)
+
+#     @staticmethod
+#     def _call_func(func: Callable, command: Command):
+#         if command.argument is not None:
+#             if isinstance(command.argument, dict):
+#                 try:
+#                     func(**command.argument)
+#                 except TypeError:
+#                     func()
+#             else:
+#                 try:
+#                     func(command.argument)
+#                 except TypeError:
+#                     func()
+#         else:
+#             func()
+
+#     @staticmethod
+#     async def async_call_func(func: Callable, command: Command):
+#         if command.argument is not None:
+#             if isinstance(command.argument, dict):
+#                 try:
+#                     await func(**command.argument)
+#                 except TypeError:
+#                     await func()
+#             else:
+#                 try:
+#                     await func(command.argument)
+#                 except TypeError:
+#                     await func()
+#         else:
+#             await func()
+
+#     def _ok_to_broadcast(self, command) -> bool:
+#         if command not in self.model.wait_queue.keys():
+#             self.model.wait_queue[command] = time.time()
+#             return True
+#         if time.time() - self.model.wait_queue.get(command, 0) > COMMAND_WAIT:
+#             self.model.wait_queue[command] = time.time()
+#             return True
+#         return False
+
 from __future__ import annotations
 
 import asyncio
@@ -30,6 +154,9 @@ class Observer:
     def __init__(self, hub):
         self.model = ObserverModel()
         self.hub = hub
+        self.hub.state_machine.bus.async_listen(
+            EVENT_STATE_CHANGED, self.async_dispatch
+        )
 
     def activate(self, init_broadcast: str = None) -> None:
         self.model.active = True
@@ -55,34 +182,34 @@ class Observer:
         cc = Command(command, _expiration, argument)
         if cc not in self.model.broadcast_queue:
             self.model.broadcast_queue.append(cc)
-        # if self.model.active:
+
+    async def async_dispatch(self):
         q: Command
         for q in self.model.broadcast_queue:
             if q.command in self.model.subscribers.keys():
-                self._dequeue_and_broadcast(q)
+                await self.async_dequeue_and_broadcast(q)
 
-    def _dequeue_and_broadcast(self, command: Command):
+    async def async_dequeue_and_broadcast(self, command: Command):
         _LOGGER.debug(
             f"ready to broadcast: {command.command} with params: {command.argument}"
         )
-        if self._ok_to_broadcast(command.command):
+        if await self.async_ok_to_broadcast(command.command):
             if command.expiration > time.time():
                 func: FunctionCall
                 for func in self.model.subscribers[command.command]:
-                    if func.call_async:
-                        _ = asyncio.run_coroutine_threadsafe(
-                            self.async_call_func(func.function, command),
-                            self.hub.state_machine.loop,
-                        ).result()
+                    if await async_iscoroutine(func):
+                        await self.async_call_func(func=func.function, command=command),
                     else:
-                        self._call_func(func.function, command)
+                        self.hub.state_machine.async_add_executor_job(
+                            self._call_func, func.function, command
+                        )
                 self.model.broadcast_queue.remove(command)
             else:
                 _LOGGER.debug(f"broadcast timed out: {command}")
                 self.model.broadcast_queue.remove(command)
 
     @staticmethod
-    def _call_func(func: Callable, command: Command):
+    def _call_func(func: Callable, command: Command) -> None:
         if command.argument is not None:
             if isinstance(command.argument, dict):
                 try:
@@ -98,7 +225,7 @@ class Observer:
             func()
 
     @staticmethod
-    async def async_call_func(func: Callable, command: Command):
+    async def async_call_func(func: Callable, command: Command) -> None:
         if command.argument is not None:
             if isinstance(command.argument, dict):
                 try:
@@ -113,7 +240,7 @@ class Observer:
         else:
             await func()
 
-    def _ok_to_broadcast(self, command) -> bool:
+    async def async_ok_to_broadcast(self, command) -> bool:
         if command not in self.model.wait_queue.keys():
             self.model.wait_queue[command] = time.time()
             return True
