f72841e2848b5f12bb02a0367af6f9840f768742
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-05-26 21:31:38 +0200

    ampmeter async update on init hub values

--- custom_components/peaqev/peaqservice/hub/sensors/ihub_sensors.py
+++ custom_components/peaqev/peaqservice/hub/sensors/ihub_sensors.py
@@ -179,7 +179,7 @@ class HubSensors:
                 is not None
                 else ""
             )
-            self.chargerobject_switch.updatecurrent()
+            await self.amp_meter.async_update()
             self.carpowersensor.value = (
                 self.state_machine.states.get(self.carpowersensor.entity).state
                 if isinstance(
