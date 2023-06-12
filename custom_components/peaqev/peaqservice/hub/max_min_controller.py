ed752edff498a4b4a61fbbbc3f288676a342bbfc
magnuselden <magnu@DESKTOP-ACV2GPT>
2023-06-12 22:26:04 +0200

    #269 add 'active'-clause to maxmin_controller

--- custom_components/peaqev/peaqservice/hub/max_min_controller.py
+++ custom_components/peaqev/peaqservice/hub/max_min_controller.py
@@ -12,6 +12,7 @@ _LOGGER = logging.getLogger(__name__)
 class MaxMinController:
     def __init__(self, hub: HomeAssistantHub):
         self.hub = hub
+        self.active: bool = hub.options.price.price_aware
         self._override_max_charge = None
         self._original_total_charge = 0
         self.override_max_charge: bool = False
@@ -24,23 +25,26 @@ class MaxMinController:
 
     @property
     def max_charge(self) -> int:
-        if self._override_max_charge is not None:
-            """overridden by frontend"""
-            return self._override_max_charge
-        if self.hub.options.max_charge > 0:
-            """set in config flow"""
-            return self.hub.options.max_charge
+        if self.active:
+            if self._override_max_charge is not None:
+                """overridden by frontend"""
+                return self._override_max_charge
+            if self.hub.options.max_charge > 0:
+                """set in config flow"""
+                return self.hub.options.max_charge
         return self._original_total_charge
 
     @property
     def remaining_charge(self) -> float:
+        if not self.active:
+            return 0
         return self.max_charge - getattr(
             self.hub.chargecontroller.session, "session_energy", 0
         )  # todo: composition
 
     async def async_override_max_charge(self, max_charge: int):
         """Overrides the max-charge with the input from frontend"""
-        if self.hub.options.price.price_aware:
+        if self.active:
             self._override_max_charge = max_charge
             await self.hub.hours.async_update_max_min(
                 max_charge=self.max_charge
@@ -55,7 +59,7 @@ class MaxMinController:
         await self.async_reset_max_charge_sensor()
 
     async def async_null_max_charge_done(self, val):
-        if val:
+        if val and self.active:
             await self.async_null_max_charge()
 
     async def async_null_max_charge(self, val=None):
@@ -68,31 +72,33 @@ class MaxMinController:
             await self.async_reset_max_charge_sensor()
 
     async def async_reset_max_charge_sensor(self) -> None:
-        try:
-            state = self.hub.state_machine.states.get("number.peaqev_max_charge")
-            if state is not None:
-                if (
-                    int(float(state.state)) == int(float(self.max_charge))
-                    or self.max_charge == 0
-                ):
-                    return
-                else:
-                    await self.async_update_sensor(self.max_charge)
-                    _LOGGER.debug(
-                        f"Resetting max charge to static value {int(float(self.max_charge))} because of {state.state}"
-                    )
-        except Exception as e:
-            _LOGGER.error(
-                f"Encountered problem when trying to reset max charge to normal: {e}"
-            )
-            return
+        if self.active:
+            try:
+                state = self.hub.state_machine.states.get("number.peaqev_max_charge")
+                if state is not None:
+                    if (
+                        int(float(state.state)) == int(float(self.max_charge))
+                        or self.max_charge == 0
+                    ):
+                        return
+                    else:
+                        await self.async_update_sensor(self.max_charge)
+                        _LOGGER.debug(
+                            f"Resetting max charge to static value {int(float(self.max_charge))} because of {state.state}"
+                        )
+            except Exception as e:
+                _LOGGER.error(
+                    f"Encountered problem when trying to reset max charge to normal: {e}"
+                )
+                return
 
     async def async_update_sensor(self, val):
-        await self.hub.state_machine.services.async_call(
-            "number",
-            "set_value",
-            {
-                "entity_id": "number.peaqev_max_charge",
-                "value": int(val),
-            },
-        )
+        if self.active:
+            await self.hub.state_machine.services.async_call(
+                "number",
+                "set_value",
+                {
+                    "entity_id": "number.peaqev_max_charge",
+                    "value": int(val),
+                },
+            )
