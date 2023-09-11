e798f33d634700a799e0ba382341c9c3cbbf23e1
Magnus Eldén <magnus.elden@outlook.com>
2023-09-11 08:46:26 +0200

    alter hub events for water heating

--- custom_components/peaqev/peaqservice/hub/hub_events.py
+++ custom_components/peaqev/peaqservice/hub/hub_events.py
@@ -1,9 +1,7 @@
 import logging
 
 events = [
-    "peaqhvac.try_heat_water_changed",
-    #"peaqhvac.currently_heating_changed",
-    #"peaqhvac.boost_changed",
+    "peaqhvac.try_heat_water_changed",    
     "peaqhvac.pre_heating_changed",
     "peaqhvac.upcoming_water_heater_warning"
 ]
@@ -14,16 +12,21 @@ class HubEvents:
     def __init__(self, hub, state_machine):
         self._hub = hub
         self.state_machine = state_machine
-        self._aux_stop = False
+        self._aux_stop = False        
+        self._event_handler_dict: dict = {}
         for e in events:
+            self._event_handler_dict[e] = False
             self.state_machine.bus.async_listen(e, self.handle_event)
             _LOGGER.debug(f"Listening to {e} for aux_stop")
 
     def handle_event(self, event):
         ret = bool(event.data.get("new", False))
-        if ret != self._aux_stop:
-            _LOGGER.debug(f"Received event {event.event_type} with data {event.data}")
-            self._aux_stop = ret
+        new_state = self._aux_stop
+        if ret != self._event_handler_dict[event.event_type]:
+            new_state = any([self._event_handler_dict.values() == True])
+        if new_state != self._aux_stop:
+            _LOGGER.debug(f"Received event {event.event_type} with data {event.data}. This changed aux stop to {self._aux_stop}")
+            self._aux_stop = new_state
             self._hub.observer.broadcast("aux stop changed")
 
     @property
