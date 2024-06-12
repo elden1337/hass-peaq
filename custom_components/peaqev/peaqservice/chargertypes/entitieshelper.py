038ebba55c6605d87bbfe01359ec704ae84052cc
magnuselden <magnu@DESKTOP-ACV2GPT>
2024-06-12 15:38:41 +0200

    fix to handle entitiesschema regardless of sorting of entities

--- custom_components/peaqev/peaqservice/chargertypes/entitieshelper.py
+++ custom_components/peaqev/peaqservice/chargertypes/entitieshelper.py
@@ -16,18 +16,20 @@ def set_entitiesmodel(
     if len(entity_schema) < 1:
         entities = get_entities_from_hass(hass=hass, domain_name=domain)
         if len(entities) < 1:
-            _LOGGER.error(f"no entities found for {domain} at {time.time()}")
+            _LOGGER.error(f'no entities found for {domain} at {time.time()}')
         else:
-            candidate = ""
+            _LOGGER.debug(f'entities: {entities} for {domain}')
+            candidate = ''
             for e in entities:
-                splitted = e.split(".")
+                splitted = e.split('.')
                 for ending in entity_endings:
                     if splitted[1].endswith(ending):
-                        candidate = splitted[1].replace(ending, "")
+                        len_ending = len(ending)
+                        candidate = splitted[1][:-len_ending]
                         break
                 if len(candidate) > 1:
                     break
-            _LOGGER.debug(f"entityschema is: {candidate} at {time.time()}")
+            _LOGGER.debug(f'entityschema is: {candidate} at {time.time()}')
             return EntitiesModel(
                 entityschema=candidate, imported_entities=entities, valid=True
             )
@@ -39,49 +41,11 @@ def get_entities_from_hass(hass: HomeAssistant, domain_name: str) -> list:
         return [
             entity_id
             for entity_id, info in entity_sources(hass).items()
-            if info["domain"] == domain_name
-            or info["domain"] == domain_name.capitalize()
-            or info["domain"] == domain_name.upper()
-            or info["domain"] == domain_name.lower()
+            if info['domain'] == domain_name
+            or info['domain'] == domain_name.capitalize()
+            or info['domain'] == domain_name.upper()
+            or info['domain'] == domain_name.lower()
         ]
     except Exception as e:
-        _LOGGER.exception(f"Could not get charger-entities from Home Assistant: {e}")
+        _LOGGER.exception(f'Could not get charger-entities from Home Assistant: {e}')
         return []
-
-
-# async def async_set_entitiesmodel(
-#     hass: HomeAssistant, domain: str, entity_endings: list, entity_schema: str
-# ) -> EntitiesModel:
-#     if len(entity_schema) < 1:
-#         entities = await async_get_entities_from_hass(hass=hass, domain_name=domain)
-#
-#         if len(entities) >= 1:
-#             candidate = ""
-#             for e in entities:
-#                 splitted = e.split(".")
-#                 for ending in entity_endings:
-#                     if splitted[1].endswith(ending):
-#                         candidate = splitted[1].replace(ending, "")
-#                         break
-#                 if len(candidate) > 1:
-#                     break
-#             _LOGGER.debug(f"Charger-entityschema is: {candidate}")
-#             return EntitiesModel(
-#                 entityschema=candidate, imported_entities=entities, valid=True
-#             )
-#     return EntitiesModel()
-#
-#
-# async def async_get_entities_from_hass(hass: HomeAssistant, domain_name: str) -> list:
-#     try:
-#         return [
-#             entity_id
-#             for entity_id, info in entity_sources(hass).items()
-#             if info["domain"] == domain_name
-#             or info["domain"] == domain_name.capitalize()
-#             or info["domain"] == domain_name.upper()
-#             or info["domain"] == domain_name.lower()
-#         ]
-#     except Exception as e:
-#         _LOGGER.exception(f"Could not get charger-entities from Home Assistant: {e}")
-#         return []
