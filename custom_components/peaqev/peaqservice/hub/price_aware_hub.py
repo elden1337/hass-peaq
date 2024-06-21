9feabcf36426be4e9bc2c5b9b00996da377f5d14
magnuselden <magnu@DESKTOP-ACV2GPT>
2024-06-21 20:14:06 +0200

    scheduler fixes

--- custom_components/peaqev/peaqservice/hub/price_aware_hub.py
+++ custom_components/peaqev/peaqservice/hub/price_aware_hub.py
@@ -27,6 +27,8 @@ class PriceAwareHub(HomeAssistantHub):
     @property
     def current_peak_dynamic(self):
         """Dynamically calculated peak to adhere to caution-hours"""
+        if self.hours.scheduler.active:
+            return self.sensors.current_peak.observed_peak
         dynamic_caution_hours: dict = self._request_sensor_lookup().get(LookupKeys.DYNAMIC_CAUTION_HOURS, {})()
         if len(dynamic_caution_hours) > 0:
             if self.now_is_caution_hour() and not getattr(self.hours.timer, 'is_override', False):
