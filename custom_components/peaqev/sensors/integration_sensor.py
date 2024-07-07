44ea8eef9f821dc476cee3a380085add28415e0f
magnuselden <magnu@DESKTOP-ACV2GPT>
2024-07-07 17:37:05 +0200

    #347

--- custom_components/peaqev/sensors/integration_sensor.py
+++ custom_components/peaqev/sensors/integration_sensor.py
@@ -5,14 +5,10 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from custom_components.peaqev.peaqservice.hub.hub import HomeAssistantHub
 
-from homeassistant.components.integration.const import (  # pylint: disable=import-error
-    METHOD_LEFT, METHOD_TRAPEZOIDAL)
-from homeassistant.components.integration.sensor import \
-    IntegrationSensor  # pylint: disable=import-error
-from homeassistant.components.sensor import \
-    SensorDeviceClass  # pylint: disable=import-error
-from homeassistant.const import (  # pylint: disable=import-error
-    ENERGY_KILO_WATT_HOUR, UnitOfTime)
+from homeassistant.components.integration.const import METHOD_TRAPEZOIDAL
+from homeassistant.components.integration.sensor import IntegrationSensor  # pylint: disable=import-error
+from homeassistant.components.sensor import SensorDeviceClass  # pylint: disable=import-error
+from homeassistant.const import (ENERGY_KILO_WATT_HOUR, UnitOfTime)
 
 import custom_components.peaqev.peaqservice.util.extensionmethods as ex
 from custom_components.peaqev.const import DOMAIN
@@ -23,17 +19,18 @@ class PeaqIntegrationCostSensor(IntegrationSensor):
     def __init__(self, hub: HomeAssistantHub, name, entry_id):
         self._entry_id = entry_id
         self.hub = hub
-        self._attr_name = f"{self.hub.hubname} {name}"
+        self._attr_name = f'{self.hub.hubname} {name}'
         self._attr_unit_of_measurement = ENERGY_KILO_WATT_HOUR
 
         super().__init__(
             integration_method=METHOD_TRAPEZOIDAL,
             name=self._attr_name,
             round_digits=5,
-            source_entity=f"sensor.{hub.hubname.lower()}_wattage_cost",
+            source_entity=f'sensor.{hub.hubname.lower()}_wattage_cost',
             unique_id=self.unique_id,
-            unit_prefix="k",
+            unit_prefix='k',
             unit_time=UnitOfTime.HOURS,
+            max_sub_interval=None
         )
 
     @property
@@ -52,19 +49,19 @@ class PeaqIntegrationCostSensor(IntegrationSensor):
     @property
     def unique_id(self):
         """Return a unique ID to use for this sensor."""
-        return f"{DOMAIN}_{self._entry_id}_{ex.nametoid(self._attr_name)}"
+        return f'{DOMAIN}_{self._entry_id}_{ex.nametoid(self._attr_name)}'
 
     @property
     def device_info(self):
         """Return information to link this entity with the correct device."""
-        return {"identifiers": {(DOMAIN, self.hub.hub_id, POWERCONTROLS)}}
+        return {'identifiers': {(DOMAIN, self.hub.hub_id, POWERCONTROLS)}}
 
 
 class PeaqIntegrationSensor(IntegrationSensor):
     def __init__(self, hub, sensor, name, entry_id):
         self._entry_id = entry_id
         self.hub = hub
-        self._attr_name = f"{self.hub.hubname} {name}"
+        self._attr_name = f'{self.hub.hubname} {name}'
         self._attr_unit_of_measurement = ENERGY_KILO_WATT_HOUR
 
         super().__init__(
@@ -73,7 +70,7 @@ class PeaqIntegrationSensor(IntegrationSensor):
             round_digits=2,
             source_entity=sensor,
             unique_id=self.unique_id,
-            unit_prefix="k",
+            unit_prefix='k',
             unit_time=UnitOfTime.HOURS,
         )
 
@@ -93,9 +90,9 @@ class PeaqIntegrationSensor(IntegrationSensor):
     @property
     def unique_id(self):
         """Return a unique ID to use for this sensor."""
-        return f"{DOMAIN}_{self._entry_id}_{ex.nametoid(self._attr_name)}"
+        return f'{DOMAIN}_{self._entry_id}_{ex.nametoid(self._attr_name)}'
 
     @property
     def device_info(self):
         """Return information to link this entity with the correct device."""
-        return {"identifiers": {(DOMAIN, self.hub.hub_id, POWERCONTROLS)}}
+        return {'identifiers': {(DOMAIN, self.hub.hub_id, POWERCONTROLS)}}
