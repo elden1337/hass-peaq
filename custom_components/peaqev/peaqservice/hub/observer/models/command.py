16aca2bac23c8940a7a3ab77b3ee2ddd018ebf3f
elden1337 <magnus.elden@outlook.com>
2026-07-31 14:29:57 +0200

    peaqservice: make Command dataclass frozen for better performance

--- custom_components/peaqev/peaqservice/hub/observer/models/command.py
+++ custom_components/peaqev/peaqservice/hub/observer/models/command.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from peaqevcore.common.models.observer_types import ObserverTypes
 
 
-@dataclass
+@dataclass(frozen=True)
 class Command:
     command: ObserverTypes
     expiration: float = None
